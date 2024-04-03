14511e679cada2d01a7a3f3c9bd5f3764dea6571
Librazionpc <nwajeizion393@gmail.com>
2024-04-03 23:19:38 +0100

    Load Balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+#Automating the task 0 using pupppet
+
+exec { 'command' :
+  command  => apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart;
+  provider => shell
+}
