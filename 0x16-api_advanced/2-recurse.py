3e76a479bc722edfedd8c74b4a0c800ec52234a5
LibrazionPc <nwajeizion393@gmail.com>
2024-06-19 14:38:18 +0100

    Fixing Some bugs

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,16 +1,13 @@
-#!/usr/bin/python3
-"""More On Api
-"""
 import requests
+""" More ON Pai's
+"""
 
 
-def recurse(subreddit, hot_list=[]):
-
+def recurse(subreddit, hot_list=[], after=None):
     """Function that queries the Reddit API and returns a list
-    containing the titles
-    of all hot articles for a given subreddit.
+    containing the titles of all hot articles for a given subreddit.
     """
-
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {'limit': 100}
     if after:
         params['after'] = after
@@ -18,8 +15,8 @@ def recurse(subreddit, hot_list=[]):
     headers = {'User-Agent': 'MyRedditApp/0.1'}
 
     try:
-        response = requests.get(url, headers=headers,
-                                params=params, allow_redirects=False)
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json()
